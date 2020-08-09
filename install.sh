bbf5488e8234aacd36296079317d3bb29160b49b
khai96_ <hvksmr1996@gmail.com>
2020-08-09 20:45:10 +0700

    Move content of gnabel.desktop out of install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -o errexit -o pipefail -o nounset
+
 ROOT_UID=0
 DEST_DIR=
 ICON_DIR=
 
+cd "$(dirname "$0")"
+
 # Destination directory
 if [ "$UID" -eq "$ROOT_UID" ]; then
-  DEST_DIR="/usr/share/"
+  DEST_DIR="/usr/bin"
   ICON_DIR="/usr/share/default"
 else
-  DEST_DIR="$HOME/.local/share"
+  DEST_DIR="$HOME/bin"
   ICON_DIR="$HOME/.icons"
 fi
 
@@ -22,24 +26,9 @@ echo "Installing..."
 pip3 install pyperclip gobject googletrans gtts pydub
 
 #Copying source files
-mkdir ${DEST_DIR}/gnabel
-cp gnabel.py ${DEST_DIR}/gnabel
-cp icon.png "$ICON_DIR"
-
-#Creating desktop entry
-cat <<EOT >> gnabel.desktop
-[Desktop Entry]
-Encoding=UTF-8
-Version=1.0
-Type=Application
-Terminal=false
-Exec=python3 ${DEST_DIR}/gnabel/gnabel.py
-Name=Gnabel
-Comment=A translation app for GTK environments based on Google Translate.
-Icon=${DEST_DIR}/gnabel/icon.png
-Keywords=gnabel;translate;translation
-EOT
-mv gnabel.desktop ${DEST_DIR}/applications
+cp -v gnabel.py "$DEST_DIR/gnabel.py"
+cp -v icon.png "$ICON_DIR/gnabel.png"
+cp -v gnabel.desktop "$DEST_DIR/applications/gnabel.desktop"
 
 #Ending
 echo ""
