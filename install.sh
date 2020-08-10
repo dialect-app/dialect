fb180faccd4981aec3c48d006794792911e35ed8
khai96_ <hvksmr1996@gmail.com>
2020-08-10 19:30:52 +0700

    Remove unused variables

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ set -o errexit -o pipefail -o nounset
 
 ROOT_UID=0
 BIN_DIR=
-ICON_DIR=
-DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 # Use containing folder of this script as working directory
 cd "$(dirname "$0")"
@@ -13,15 +11,12 @@ cd "$(dirname "$0")"
 # Destination directory
 if [ "$UID" -eq "$ROOT_UID" ]; then
   BIN_DIR="/usr/bin"
-  ICON_DIR="/usr/share/default"
 else
   BIN_DIR="$HOME/.local/bin"
-  ICON_DIR="$DATA_HOME/icons"
 fi
 
 # Make sure destination directories exist
 mkdir -pv "$BIN_DIR"
-mkdir -pv "$ICON_DIR"
 
 #Starting
 echo "NOTE: Gnabel requires Python 3 and Pip. If they are not present, you should be able to install them using your distribution's package"
