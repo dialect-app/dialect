bbfd18a3eb8220527abe893cd20632994d979e0c
gi-lom <66316639+gi-lom@users.noreply.github.com>
2020-08-12 09:01:12 +0200

    Remove pyperclip dependency

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ echo ""
 echo "Installing..."
 
 #Installing Python libraries
-pip3 install pyperclip gobject googletrans gtts pydub
+pip3 install gobject googletrans gtts pydub
 
 #Copying source files
 cp -v gnabel.py "$BIN_DIR/gnabel"
