5a837f0fa78a81c3cfca4c021f7022f97705b673
khai96_ <hvksmr1996@gmail.com>
2020-08-12 08:05:31 +0700

    Fix "NameError: name 'XdgConfigHome' is not defined"
    @gi-lom made a mistake while merging https://github.com/gi-lom/gnabel/pull/7/
    
    ref https://github.com/gi-lom/gnabel/pull/7/commits/ba2f141b5ae0cc97a70e0ac4357f3a5e3f38d028#diff-62ade00c6da0798c62ef75b31cd74e79L23
    ref https://github.com/gi-lom/gnabel/pull/7/commits/ba2f141b5ae0cc97a70e0ac4357f3a5e3f38d028#r468529558

--- gnabel.py
+++ gnabel.py
@@ -21,6 +21,7 @@ TransNumber = 10  # number of translations to save in history
 LanNumber = 8  # number of language tuples to save in history
 ButtonLength = 65  # length of language buttons
 ButtonNumLanguages = 3  # number of language buttons
+XdgConfigHome = GLib.get_user_config_dir()
 SettingsFile = os.path.join(XdgConfigHome, 'gnabel', 'settings.json')
 
 
