80337d1143dceed939efbb0c8307aea314c59d71
gi-lom <66316639+gi-lom@users.noreply.github.com>
2020-08-12 13:33:36 +0200

    Added --user on pip3 command

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ echo ""
 echo "Installing..."
 
 #Installing Python libraries
-sudo pip3 install gobject googletrans gtts pydub
+pip3 install gobject googletrans gtts pydub --user
 
 #Copying source files
 cp -v gnabel.py "$BIN_DIR/gnabel"
