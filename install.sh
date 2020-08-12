11b56b4d38531964daee29bd97c4795a6a32a04b
khai96_ <hvksmr1996@gmail.com>
2020-08-12 07:58:38 +0700

    Stop using xdg-icon-resource
    `xdg-icon-resource` does not support `svg` extension and `--size scalable`

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -o errexit -o pipefail -o nounset
 ROOT_UID=0
 BIN_DIR=
 DESKTOP_DIR=
+ICON_DIR=
 DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 # Use containing folder of this script as working directory
@@ -14,14 +15,17 @@ cd "$(dirname "$0")"
 if [ "$UID" -eq "$ROOT_UID" ]; then
   BIN_DIR="/usr/bin"
   DESKTOP_DIR="/usr/share/applications"
+  ICON_DIR="/usr/share/icons/hicolor/scalable/apps"
 else
   BIN_DIR="$HOME/.local/bin"
   DESKTOP_DIR="$DATA_HOME/applications"
+  ICON_DIR="$DATA_HOME/icons/hicolor/scalable/apps"
 fi
 
 # Make sure destination directories exist
 mkdir -pv "$BIN_DIR"
 mkdir -pv "$DESKTOP_DIR"
+mkdir -pv "$ICON_DIR"
 
 #Starting
 echo "NOTE: Gnabel requires Python 3 and Pip. If they are not present, you should be able to install them using your distribution's package"
@@ -34,9 +38,7 @@ pip3 install pyperclip gobject googletrans gtts pydub
 #Copying source files
 cp -v gnabel.py "$BIN_DIR/gnabel"
 cp -v gnabel.desktop "$DESKTOP_DIR/gnabel.desktop"
-
-#Install icon
-xdg-icon-resource install --size 64 icon.svg gnabel
+cp -v icon.svg "$ICON_DIR/gnabel.svg"
 
 #Ending
 echo ""
