2b0d1ab72928ac41557be7720b542325b9d36df0
khai96_ <hvksmr1996@gmail.com>
2020-08-12 08:07:47 +0700

    Import GLib

--- gnabel.py
+++ gnabel.py
@@ -7,7 +7,7 @@ from io import BytesIO
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, Gio
+from gi.repository import Gtk, Gdk, Gio, GLib
 
 from googletrans import LANGUAGES, Translator
 from gtts import gTTS, lang
