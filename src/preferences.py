f0ea9f8063f87c8907bb3eda7f4715c5e7a29c98
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2021-02-18 09:49:05 -0500

    Make insensitive the whole editing box when validating backend instance URL

--- src/preferences.py
+++ src/preferences.py
@@ -30,6 +30,7 @@ class DialectPreferencesWindow(Handy.PreferencesWindow):
     backend_instance_edit = Gtk.Template.Child()
     backend_instance_save = Gtk.Template.Child()
     backend_instance_reset = Gtk.Template.Child()
+    backend_instance_edit_box = Gtk.Template.Child()
     search_provider = Gtk.Template.Child()
 
     def __init__(self, parent, settings, **kwargs):
@@ -175,13 +176,13 @@ class DialectPreferencesWindow(Handy.PreferencesWindow):
 
     def __validate_new_backend_instance(self, url):
         def spinner_start():
-            self.backend_instance_save.set_sensitive(False)
+            self.backend_instance_edit_box.set_sensitive(False)
             self.backend_instance_save.remove(self.instance_save_image)
             self.backend_instance_save.add(self.instance_save_spinner)
             self.instance_save_spinner.start()
 
         def spinner_end():
-            self.backend_instance_save.set_sensitive(True)
+            self.backend_instance_edit_box.set_sensitive(True)
             self.backend_instance_save.remove(self.instance_save_spinner)
             self.backend_instance_save.add(self.instance_save_image)
             self.instance_save_spinner.stop()
