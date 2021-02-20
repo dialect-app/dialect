ea6928cadc7781cd5ae1c15e7879e181591f78ff
Mufeed Ali <fushinari@protonmail.com>
2021-02-20 19:58:15 +0530

    Allow working with 4.0.0rc1
    Except it doesn't... See:
    https://github.com/gi-lom/dialect/issues/133#issuecomment-782687627

--- src/translators/gtrans.py
+++ src/translators/gtrans.py
@@ -24,12 +24,16 @@ class GTranslator(TranslatorBase):
 
     def detect(self, src_text):
         try:
+            if callable(getattr(self._translator, 'detect_legacy', None)):
+                return self._translator.detect_legacy(src_text)
             return self._translator.detect(src_text)
         except Exception as exc:
             raise TranslationError(exc) from exc
 
     def translate(self, src_text, src, dest):
         try:
+            if callable(getattr(self._translator, 'translate_legacy', None)):
+                return self._translator.translate_legacy(src_text)
             return self._translator.translate(src_text, src=src, dest=dest)
         except Exception as exc:
             raise TranslationError(exc) from exc
