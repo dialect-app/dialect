43426cbd45fa1977542a06fd1579c2129149453d
Rafael Mardojai CM <email@rafaelmardojai.com>
2024-07-28 18:02:28 -0500

    Copyright statements: Only use the first contribution year
    We don't want to keep track of these statements, so let's simplify them

--- dialect/widgets/__init__.py
+++ dialect/widgets/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2020-2022 Mufeed Ali
-# Copyright 2020-2022 Rafael Mardojai CM
+# Copyright 2020 Mufeed Ali
+# Copyright 2020 Rafael Mardojai CM
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from dialect.widgets.lang_selector import LangSelector  # noqa
--- dialect/widgets/lang_selector.py
+++ dialect/widgets/lang_selector.py
@@ -1,5 +1,5 @@
-# Copyright 2020-2022 Mufeed Ali
-# Copyright 2020-2022 Rafael Mardojai CM
+# Copyright 2020 Mufeed Ali
+# Copyright 2020 Rafael Mardojai CM
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import re
